0b85bee428a32804f866d9ada171ee1606fca0cd
fabian <fabianm536@gmail.com>
2019-02-28 14:37:02 +0100

    BD

--- php/print.php
+++ php/print.php
@@ -2,6 +2,7 @@
 require('fpdf181/fpdf.php');
 
 define('UPLOAD_DIR', 'tmp/');
+//$logo= '../img/logo.png';
 
 $img1 = $_FILES['picture1']['tmp_name'];
 $img2 = $_FILES['picture2']['tmp_name'];
@@ -16,9 +17,12 @@ copy($img3, $file3);
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
-
-$pdf->Image($file1,15,15,180);
-$pdf->Image($file2,15,135,180);
+//$pdf->Image($logo,171.5,-0.1,24.7);
+//$pdf->SetDrawColor(218,190,134);
+//$pdf->Line(10, 26, 200, 26);
+//$pdf->Line(10, 285, 200, 285);
+$pdf->Image($file1,15,30,180);
+$pdf->Image($file2,15,150,180);
 $pdf->Image($file3,15,228,70);
 
 $pdf->Output(null, 'foobar-' . time() . '.pdf');
